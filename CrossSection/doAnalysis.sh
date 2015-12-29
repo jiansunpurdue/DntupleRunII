a628d76cfea07cf211f4ee933ab0bf42358513c1
ginnocen <ginnocen@cern.ch>
2015-12-29 15:01:30 +0100

    new pbpb file

--- CrossSection/doAnalysis.sh
+++ CrossSection/doAnalysis.sh
@@ -15,8 +15,8 @@ LABELPP="PP"
 OUTPUTFILEPP="hPtSpectrumDzeroPP.root"
 
 INPUTMCPbPb="/data/dmeson2015/MCDntuple/ntD_20151115_DfinderMC_20151110_EvtMatching_Pythia_TuneZ2_5020GeV_GENSIM_75x_1015_20151110_ppGlobaTrackingPPmenuHFlowpuv11_7415_v20_1116_Pthat5_15_35merged.root"
-INPUTDATAPbPb="/data/HeavyFlavourRun2/DfinderData_PbPb_20151220_dPt10tkPt2p5_D0Dstar3p5p_hltfix/merged_ntuple.root"
-LUMIPbPb=0.0000095
+INPUTDATAPbPb="/afs/cern.ch/work/g/ginnocen/HeavyFlavourRun2/crab_DfinderData_PbPb_20151227_dPt10tkPt2p5_D0Dstar3p5p_CameliaJSON/merged_ntuple.root"
+LUMIPbPb=0.000014930
 ISMCPbPb=0
 ISDOWEIGHTPbPb=1
 CUTPbPb="Dy>-1.&&Dy<1.&&(Dtrk1Pt>8.5&&Dtrk2Pt>8.5)&&((Dpt<20&&(DsvpvDistance/DsvpvDisErr)>4.50&&Dchi2cl>0.05&&Dalpha<0.12)||(Dpt>20&&Dpt<40&&(DsvpvDistance/DsvpvDisErr)>2.5&&Dchi2cl>0.2&&Dalpha<0.12)||(Dpt>40&&(DsvpvDistance/DsvpvDisErr)>2.5&&Dchi2cl>0.05&&Dalpha<0.12))"
