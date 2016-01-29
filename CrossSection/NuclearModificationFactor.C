241488fcdefaa5f92782960d9b94d43b8941fec6
ginnocen <ginnocen@cern.ch>
2016-01-29 13:47:07 +0100

    plot options

--- CrossSection/NuclearModificationFactor.C
+++ CrossSection/NuclearModificationFactor.C
@@ -23,12 +23,13 @@ void NuclearModificationFactor(TString inputPP="CrossSectionFONLLPP.root", TStri
   hNuclearModification->Scale(1/208./208.);
   hNuclearModification->Divide(hSigmaPPStat);
   
-  TCanvas*canvasRAA=new TCanvas("canvasRAA","canvasRAA",500,500);
+  TCanvas*canvasRAA=new TCanvas("canvasRAA","canvasRAA",550,500);
   canvasRAA->cd();
-  TH2F* hemptyEff=new TH2F("hemptyEff","",50,0.,120.,10.,0,1.5);  
+  canvasRAA->SetLogx();
+  TH2F* hemptyEff=new TH2F("hemptyEff","",50,10.,120.,10.,0,1.5);  
   hemptyEff->GetXaxis()->CenterTitle();
   hemptyEff->GetYaxis()->CenterTitle();
-  hemptyEff->GetYaxis()->SetTitle("Nuclear Modification");
+  hemptyEff->GetYaxis()->SetTitle("D^{0} R_{AA}");
   hemptyEff->GetXaxis()->SetTitle("p_{T} (GeV/c)");
   hemptyEff->GetXaxis()->SetTitleOffset(1.);
   hemptyEff->GetYaxis()->SetTitleOffset(1.1);
@@ -44,6 +45,24 @@ void NuclearModificationFactor(TString inputPP="CrossSectionFONLLPP.root", TStri
   hemptyEff->SetMinimum(0.);
   hemptyEff->Draw();
   hNuclearModification->Draw("same");
+  TLatex * tlatexeff=new TLatex(0.1612903,0.8525793,"CMS Preliminary     PbPb #sqrt{s}= 5.02 TeV");
+  tlatexeff->SetNDC();
+  tlatexeff->SetTextColor(1);
+  tlatexeff->SetTextFont(42);
+  tlatexeff->SetTextSize(0.04);
+  tlatexeff->Draw();
+  TLatex * tlatexeff2=new TLatex(0.1612903,0.7925793,"Centrality 0-100%");
+  tlatexeff2->SetNDC();
+  tlatexeff2->SetTextColor(1);
+  tlatexeff2->SetTextFont(42);
+  tlatexeff2->SetTextSize(0.04);
+  tlatexeff2->Draw();
+  TLatex * tlatexeff3=new TLatex(0.1612903,0.7325793,"D^{0} #rightarrow K#pi");
+  tlatexeff3->SetNDC();
+  tlatexeff3->SetTextColor(1);
+  tlatexeff3->SetTextFont(42);
+  tlatexeff3->SetTextSize(0.04);
+  tlatexeff3->Draw();
   canvasRAA->SaveAs("canvasRAA.pdf");
 }
 
